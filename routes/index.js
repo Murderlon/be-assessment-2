7a0b76ba6c8f117194567eab119bcc5d694b9829
Murderlon <hello@murderlon.me>
2018-04-07 18:33:26 +0200

    add login/logout functionality

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,24 @@ const createError = require('../lib/createError')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) =>
+router.get('/', (req, res, next) => {
+  console.log(req.user)
   fs.readdir('./static/img', (err, images) =>
     err ? next(createError(500)) : res.render('index', { images }))
-)
+})
 
-router.get('/register', (req, res) => res.render('register', {error: ''}))
+router.get('/register', (req, res) => res.render('register', { error: null }))
+
+router.get('/login', (req, res) => res.render('login', {error: null}))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.redirect('/')
+})
 
 router.post('/register', (req, res, next) => {
   const { username, password } = req.body
