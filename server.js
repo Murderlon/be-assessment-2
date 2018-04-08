36d353464d64f1a3b91901279fd3e6086e8f128a
Murderlon <hello@murderlon.me>
2018-04-08 12:29:55 +0200

    abstract passport logic into lib

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const logger = require('morgan')
 
 const routes = require('./routes')
 const notFound = require('./routes/notFound')
 const error = require('./routes/error')
-const User = require('./models/user')
 
 const port = process.env.PORT || 1900
 
+require('./lib/passport')(passport)
 mongoose.connect(process.env.DB_URL)
 
 express()
@@ -36,7 +35,3 @@ express()
   .use(notFound)
   .use(error)
   .listen(port)
-
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
