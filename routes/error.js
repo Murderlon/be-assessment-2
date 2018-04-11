988c0e26ce4ced6f0a36d0a241e1543624257a7b
Murderlon <hello@murderlon.me>
2018-04-11 17:26:21 +0200

    improve res for error route

--- routes/error.js
+++ routes/error.js
@@ -1,9 +1,8 @@
-
 module.exports = (err, req, res, next) => {
   const code = err.status || 500
-  res.status(code).render('error', {
-    id: code,
-    title: err,
-    detail: err
+  res.format({
+    json: () => res.status(code).json(err),
+    html: () =>
+      res.status(code).render('error', { id: code, title: err, detail: err })
   })
 }
